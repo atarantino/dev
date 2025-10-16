48760b5341add964db85f5776e9cec0762c68299
Adam Tarantino <tarantino.adam@hey.com>
2025-10-16 13:24:56 -0700

    cleanup

--- lua/plugins/calendar.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "mattn/calendar-vim",
-  config = function()
-    -- Calendar settings
-    vim.g.calendar_monday = 0  -- Start week on Sunday
-    vim.g.calendar_weeknm = 1  -- Show week numbers
-    vim.g.calendar_mark = "left-fit"  -- Mark style
-  end,
-}
